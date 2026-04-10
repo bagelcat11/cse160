237f82c3f3ac20617fb40985c98138ee0a8d7fd4
bagelcat11 <lynn.g0032@gmail.com>
2026-04-10 16:23:57 -0700

    asgn1 part 3 re-org done

--- asgn1/asgn1.js
+++ asgn1/asgn1.js
@@ -1,4 +1,5 @@
 // -- Vertex shader program --
+// we use \n and + so that errors give us line numbers
 var VSHADER_SOURCE = 
   'attribute vec4 a_Position;\n' +  // attributes: external vars that can vary for each vertex
   'attribute float a_PointSize;\n'+
@@ -10,22 +11,34 @@ var VSHADER_SOURCE =
 
 // -- Fragment shader program --
 var FSHADER_SOURCE =
+  'precision mediump float;\n' +
+  'uniform vec4 u_FragColor;\n' + // uniform var: external var that is the same for all fragments
+  '\n' +
   'void main() {\n' +
-  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + // Set the point color
+  '  gl_FragColor = u_FragColor;\n' + // Set the point color
   '}\n';
 
+// -- GLOBALS --
+let canvas;
+let gl;
+let a_Position;
+let a_PointSize;
+let u_FragColor;
 
-function main() {
+// -- Setup helpers --
+function setupWebGL() {
   // Retrieve <canvas> element
-  var canvas = document.getElementById("webgl");
+  canvas = document.getElementById("webgl");
 
   // Get the rendering context for WebGL
-  var gl = getWebGLContext(canvas);
+  gl = getWebGLContext(canvas);
   if (!gl) {
     console.log("Failed to get the rendering context for WebGL");
     return;
   }
+}
 
+function connectVariablesToGLSL() {
   // Initialize shaders
   if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
     console.log("Failed to intialize shaders");
@@ -34,36 +47,60 @@ function main() {
 
   // get storage locations of attribute vars from gl.program, which can
   // only be referenced after initShaders is called
-  var a_Position = gl.getAttribLocation(gl.program, "a_Position");
-  var a_PointSize = gl.getAttribLocation(gl.program, "a_PointSize");
+  a_Position = gl.getAttribLocation(gl.program, "a_Position");
+  a_PointSize = gl.getAttribLocation(gl.program, "a_PointSize");
+  // get location of uniform var
+  u_FragColor = gl.getUniformLocation(gl.program, "u_FragColor");
+}
 
-  // set up click listener to pass click positions to attribute var
-  // defining it like this means on click, anon(event) is called, then our click function with our params
-  canvas.onmousedown = function(event) { click(event, gl, canvas, a_Position); };
+
+// -- MAIN --
+function main() {
+  setupWebGL();
+  connectVariablesToGLSL();
+
+  // set up click listener to call click handler
+  canvas.onmousedown = click;
 
   // pass a position to it
   gl.vertexAttrib3f(a_Position, 0.0, 0.5, 0.0); // 3f so that 4th homogenous coord is default 1.0
-  gl.vertexAttrib1f(a_PointSize, 5.0);
+  gl.vertexAttrib1f(a_PointSize, 10.0);
 
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
-
-  // Draw a point
-  // mode, first vertex to draw from, number of vertices to draw
-  gl.drawArrays(gl.POINTS, 0, 1);
 }
 
-// -- Helper funcs/things --
+
+// -- Extra helper funcs/things --
 
 // global array for mouse click points
 // we need to store all points since buffer gets cleared on draw
 var g_points = [];
+var g_colors = [];
 
 // define click handler
-function click(event, gl, canvas, a_Position) {
+function click(event) {
+  let [x, y] = convertCoordinatesEventToGL(event);
+
+  // push xy components
+  g_points.push([x, y]);
+
+  // push colors
+  if (x >= 0 && y > 0) {  // Q1 = red
+    g_colors.push([1.0, 0.0, 0.0, 1.0]);
+  } else if (x < 0 && y < 0) {  // Q3 = green
+    g_colors.push([0.0, 1.0, 0.0, 1.0]);
+  } else {  // others = white
+    g_colors.push([0.0, 0.0, 0.0, 0.0]);
+  }
+  
+  renderAllShapes();
+}
+
+function convertCoordinatesEventToGL(event) {
   // transform browser coords -> canvas coords -> webgl coords
   var x = event.clientX;
   var y = event.clientY;
@@ -71,14 +108,21 @@ function click(event, gl, canvas, a_Position) {
   x = ((x - rect.left) - canvas.height / 2) / (canvas.height / 2);
   y = (canvas.width / 2 - (y - rect.top)) / (canvas.width / 2);
 
-  // push components to array and clear screen
-  g_points.push([x, y]);
+  return [x, y]
+}
+
+function renderAllShapes() {
+  // clear canvas
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  // for each point, set position attribute with x and y and then draw
+  // for each point, set position attribute with x and y, color uniform, and then draw
   for (let i = 0; i < g_points.length; i++) {
-    var xy = g_points[i]
-    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0)
+    var xy = g_points[i];
+    var rgba = g_colors[i];
+    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+    // Draw a point
+    // mode, first vertex to draw from, number of vertices to draw
     gl.drawArrays(gl.POINTS, 0, 1);
   }
 }

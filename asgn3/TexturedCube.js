330723c87c780ee6a607b154eb89de46f26e8f4b
bagelcat11 <lynn.g0032@gmail.com>
2026-05-10 00:59:37 -0700

    performance improvements

--- asgn3/TexturedCube.js
+++ asgn3/TexturedCube.js
@@ -7,6 +7,21 @@ class TexturedCube extends Shape {
     this.matrix = new Matrix4();
 
     this.setUpBuffer();
+
+    this.verticesAndUVs = new Float32Array([
+      // top
+      -0.5,-0.5,-0.5,0,0, 0.5,-0.5,-0.5,1,0, -0.5,0.5,-0.5,0,1, 0.5,0.5,-0.5,1,1, 0.5,-0.5,-0.5,1,0, -0.5,0.5,-0.5,0,1,
+      // right
+      0.5,-0.5,-0.5,1,1, 0.5,0.5,-0.5,1,0, 0.5,-0.5,0.5,0,1, 0.5,0.5,0.5,0,0, 0.5,0.5,-0.5,1,0, 0.5,-0.5,0.5,0,1,
+      // left
+      -0.5,-0.5,-0.5,1,1, -0.5,0.5,-0.5,1,0, -0.5,-0.5,0.5,0,1, -0.5,0.5,0.5,0,0, -0.5,0.5,-0.5,1,0, -0.5,-0.5,0.5,0,1,
+      // front
+      -0.5,-0.5,0.5,0,1, 0.5,-0.5,0.5,0,0, -0.5,-0.5,-0.5,1,1, 0.5,-0.5,-0.5,1,0, 0.5,-0.5,0.5,0,0, -0.5,-0.5,-0.5,1,1,
+      // back
+      -0.5,0.5,0.5,0,1, 0.5,0.5,0.5,0,0, -0.5,0.5,-0.5,1,1, 0.5,0.5,-0.5,1,0, 0.5,0.5,0.5,0,0, -0.5,0.5,-0.5,1,1,
+      // bottom
+      -0.5,-0.5,0.5,0,0, 0.5,-0.5,0.5,1,0, -0.5,0.5,0.5,0,1, 0.5,0.5,0.5,1,1, 0.5,-0.5,0.5,1,0, -0.5,0.5,0.5,0,1,
+    ]);
   }
 
   setUpBuffer() {
@@ -20,22 +35,23 @@ class TexturedCube extends Shape {
   
 
   render() {
-    //top
-    this.drawTexturedRect([-0.5,-0.5,-0.5,0,0, 0.5,-0.5,-0.5,1,0, -0.5,0.5,-0.5,0,1, 0.5,0.5,-0.5,1,1,], this.textureNum);
-    // right
-    this.drawTexturedRect([0.5,-0.5,-0.5,1,1, 0.5,0.5,-0.5,1,0, 0.5,-0.5,0.5,0,1, 0.5,0.5,0.5,0,0,], this.textureNum);
-    // left
-    this.drawTexturedRect([-0.5,-0.5,-0.5,1,1, -0.5,0.5,-0.5,1,0, -0.5,-0.5,0.5,0,1, -0.5,0.5,0.5,0,0,], this.textureNum);
-    // front
-    this.drawTexturedRect([-0.5,-0.5,0.5,0,1, 0.5,-0.5,0.5,0,0, -0.5,-0.5,-0.5,1,1, 0.5,-0.5,-0.5,1,0,], this.textureNum);
-    // back
-    this.drawTexturedRect([-0.5,0.5,0.5,0,1, 0.5,0.5,0.5,0,0, -0.5,0.5,-0.5,1,1, 0.5,0.5,-0.5,1,0,], this.textureNum);
-    // bottom
-    this.drawTexturedRect([-0.5,-0.5,0.5,0,0, 0.5,-0.5,0.5,1,0, -0.5,0.5,0.5,0,1, 0.5,0.5,0.5,1,1,], this.textureNum);
+    this.drawTexturedRect(this.verticesAndUVs, this.textureNum);
+    // //top
+    // this.drawTexturedRect([-0.5,-0.5,-0.5,0,0, 0.5,-0.5,-0.5,1,0, -0.5,0.5,-0.5,0,1, 0.5,0.5,-0.5,1,1,], this.textureNum);
+    // // right
+    // this.drawTexturedRect([0.5,-0.5,-0.5,1,1, 0.5,0.5,-0.5,1,0, 0.5,-0.5,0.5,0,1, 0.5,0.5,0.5,0,0,], this.textureNum);
+    // // left
+    // this.drawTexturedRect([-0.5,-0.5,-0.5,1,1, -0.5,0.5,-0.5,1,0, -0.5,-0.5,0.5,0,1, -0.5,0.5,0.5,0,0,], this.textureNum);
+    // // front
+    // this.drawTexturedRect([-0.5,-0.5,0.5,0,1, 0.5,-0.5,0.5,0,0, -0.5,-0.5,-0.5,1,1, 0.5,-0.5,-0.5,1,0,], this.textureNum);
+    // // back
+    // this.drawTexturedRect([-0.5,0.5,0.5,0,1, 0.5,0.5,0.5,0,0, -0.5,0.5,-0.5,1,1, 0.5,0.5,-0.5,1,0,], this.textureNum);
+    // // bottom
+    // this.drawTexturedRect([-0.5,-0.5,0.5,0,0, 0.5,-0.5,0.5,1,0, -0.5,0.5,0.5,0,1, 0.5,0.5,0.5,1,1,], this.textureNum);
   }
 
   drawTexturedRect(cornersAndUVs, textureNum) {
-    cornersAndUVs = new Float32Array(cornersAndUVs);
+    // cornersAndUVs = new Float32Array(cornersAndUVs);
     gl.bufferData(gl.ARRAY_BUFFER, cornersAndUVs, gl.DYNAMIC_DRAW);
     let FSIZE = cornersAndUVs.BYTES_PER_ELEMENT;
 
@@ -57,6 +73,7 @@ class TexturedCube extends Shape {
     gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
 
     // using a strip means the last 2 vertices of the prev tri are used for the next tri
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    // gl.drawArrays(gl.TRIANGLE_STRIP, 0, cornersAndUVs.length / 5);
+    gl.drawArrays(gl.TRIANGLES, 0, cornersAndUVs.length / 5);
   }
 }

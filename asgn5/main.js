64c8c6914eb3114b176b7e8690449930733bd6f9
bagelcat11 <lynn.g0032@gmail.com>
2026-05-30 17:57:29 -0700

    test light and gltf

--- asgn5/main.js
+++ asgn5/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
 
 // -- setup --
 const scene = new THREE.Scene();
@@ -11,16 +12,39 @@ document.body.appendChild(renderer.domElement);
 // -- define scene --
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const shapes = [];
+
+const texLoader = new THREE.TextureLoader();
+const lokiTex = texLoader.load("img/test_loki.png", (t) => {
+    const material = new THREE.MeshBasicMaterial({map: lokiTex});
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.y = -1;
+    scene.add(cube);
+    shapes.push(cube);
+});
+
+const gltfLoader = new GLTFLoader();
+gltfLoader.load("model/Dingus the cat.glb", (gltf) => {
+    const root = gltf.scene;
+    scene.add(root);
+    shapes.push(root);
+});
+
+const light = new THREE.DirectionalLight(0xFFFFFF, 10);
+light.position.set(0,0,10);
+light.target.position.set(0,0,0);
+scene.add(light);
+scene.add(light.target);
 
 camera.position.z = 5;
 
 // -- update loop --
 function animate(time) {
-    cube.rotation.x = time / 2000;
-    cube.rotation.y = time / 1000;
-    renderer.render( scene, camera );
+    for (let i = 0; i < shapes.length; i++) {
+        shapes[i].rotation.x = Math.sin(time / 1000) / 5;
+        shapes[i].rotation.y = time / 1000;
+    }
+
+    renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
